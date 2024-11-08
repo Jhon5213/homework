1df3d6a24908927be2756a59ecf23451b3979abf
Evgeniy <microlabx7@gmail.com>
2024-11-08 21:10:04 +0400

    first

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Evgeniy'
+print(name)
+age = 30
+print(age)
+new_age = 18
+print(new_age)
+is_student = True
+print(is_student)
---
+++ module_1_6.py
@@ -0,0 +1,4 @@
+completed_task_count = 12
+number_of_hours_spent = 1.5
+course_title ='Python'
+time_one_task = complted_task_count/number_of_hours_sept
